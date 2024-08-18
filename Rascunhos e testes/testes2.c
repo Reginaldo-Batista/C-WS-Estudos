71c873a531516e6628a9f4871ba45fe1e591c6bb
Reginaldo Batista <reginaldovlog@gmail.com>
2024-08-18 17:14:41 -0300

    adicionado projeto_vetor

--- Rascunhos e testes/testes2.c
+++ Rascunhos e testes/testes2.c
@@ -4,19 +4,19 @@
 #include <stdlib.h>
 #include <string.h>
 
-struct Personagem{
+typedef struct Personagem{
     int valorElemento;
     int indiceValor;
-};
+}personagem;
 
-struct vetor{
-    char nome[30];
+typedef struct vetor{
+    char nomeVetor[30];
     int tamanho;
     struct Personagem *arrayOrdenado;
-};
+}vetor;
 
 // Função para realizar o Bubble Sort
-void bubbleSort_Vetor(struct Personagem *arr, int n) {
+void bubbleSort_Vetor(personagem *arr, int n) {
     struct Personagem temp;
     for (int i = 0; i < n-1; i++) {
         for (int j = 0; j < n-i-1; j++) {
@@ -29,7 +29,7 @@ void bubbleSort_Vetor(struct Personagem *arr, int n) {
     }
 }
 
-void atribuindoValoresArray_Vetor(struct vetor *vetor){
+void atribuindoValoresArray_Vetor(vetor *vetor){
     int tamanho = vetor->tamanho;
     printf("Insira os valores de cada posicao do vetor %s\n\n", vetor->nome);
     for (int i = 0; i < tamanho; i++) {
@@ -42,7 +42,7 @@ void atribuindoValoresArray_Vetor(struct vetor *vetor){
     bubbleSort_Vetor(vetor->arrayOrdenado, tamanho);
 }
 
-void construtorVetor(struct vetor *vetor, char nome[30], int tamanho){
+void construtorVetor(vetor *vetor, char nome[30], int tamanho){
     strcpy(vetor->nome, nome);
     vetor->tamanho = tamanho;
     vetor->arrayOrdenado = (struct Personagem *) malloc(tamanho * sizeof(struct Personagem));
@@ -60,12 +60,12 @@ void construtorVetor(struct vetor *vetor, char nome[30], int tamanho){
 
 int main() {
 
-    struct vetor pirulei;
-    construtorVetor(&pirulei, "Jorginho", 5);
+    vetor vetor;
+    construtorVetor(&vetor, "Jorginho", 5);
 
     printf("Valores do vetor ordenados:\n");
-    for (int i = 0; i < pirulei.tamanho; i++) {
-        printf("%d ", pirulei.arrayOrdenado[i].valorElemento); 
+    for (int i = 0; i < vetor.tamanho; i++) {
+        printf("%d ", vetor.arrayOrdenado[i].valorElemento); 
     }
     printf("\n\n");
 
