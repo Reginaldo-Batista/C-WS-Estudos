ee70bb4106dfe4fb28de34c251e5477994eba74c
Reginaldo Batista <reginaldovlog@gmail.com>
2024-08-21 09:11:43 -0300

    inicio de desenvolvimento de uma linked list em c

--- estrutura_de_dados/lista_ligada_simples.c
+++ estrutura_de_dados/lista_ligada_simples.c
@@ -1,8 +1,34 @@
 #include <stdio.h>
+#include <stdlib.h>
+
+typedef struct Node {
+
+    int valor;
+    struct Node *proximo;
+
+}node;
+
+void startInsertNode(node *inicio, int novoValor) {
+
+    node *novoNode = (node*) malloc(sizeof(node));
+
+    if (novoNode) {
+        novoNode->valor = novoValor;
+        novoNode->proximo = inicio->proximo;
+        inicio->proximo = novoNode;
+    }
+    else
+        printf("Erro ao alocar memoria!\n");
+
+}
 
 int main() {
 
-    
+    node inicio;
+
+    startInsertNode(&inicio, 21);
+
+    printf("valor: %d", inicio.proximo->valor);
 
     return 0;
 }
