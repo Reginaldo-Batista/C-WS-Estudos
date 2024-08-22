dfacc4c743bd798729c42171929b633d8ac7fdf3
Reginaldo Batista <reginaldovlog@gmail.com>
2024-08-22 17:55:27 -0300

    adicionada a função de inserir elementos no meio da lista ligada

--- estrutura_de_dados/lista_ligada_simples.c
+++ estrutura_de_dados/lista_ligada_simples.c
@@ -1,5 +1,8 @@
 #include <stdio.h>
 #include <stdlib.h>
+#define fimMenu 4
+
+unsigned int quantidadeDeNodes = 0;
 
 typedef struct Node {
 
@@ -33,6 +36,7 @@ void inserirNodeInicio(node **inicio, int novoValor) {
         novoNode->valor = novoValor;
         novoNode->proximo = *inicio;
         *inicio = novoNode;
+        quantidadeDeNodes++;
     }
     else
         printf("Erro ao alocar memoria!\n");
@@ -49,6 +53,7 @@ void inserirNodeFim(node **inicio, int novoValor) {
         
         if (*inicio == NULL) {
             *inicio = novoNode;
+            quantidadeDeNodes++;
         }
         else {
             node *auxNode = *inicio;
@@ -56,6 +61,47 @@ void inserirNodeFim(node **inicio, int novoValor) {
                 auxNode = auxNode->proximo;
             }
             auxNode->proximo = novoNode;
+            quantidadeDeNodes++;
+        }
+
+    }
+    else
+        printf("Erro ao alocar memoria!\n");
+
+}
+
+void inserirNode(node **inicio, unsigned int indice, int novoValor) {
+
+    node *novoNode = (node*) malloc(sizeof(node));
+
+    if (novoNode) {
+        novoNode->valor = novoValor;
+        
+        if (*inicio == NULL) {
+            novoNode->proximo = NULL;
+            *inicio = novoNode;
+            quantidadeDeNodes++;
+        }
+        else if (indice > quantidadeDeNodes - 1 || indice <= 0) {
+            printf("Posicao invalida!\n");
+            free(novoNode);
+        }
+        else {
+            node *auxNode1 = *inicio;
+            node *auxNode2 = *inicio;
+            unsigned int auxIndiceNode1 = 0;
+            unsigned int auxIndiceNode2 = 0;
+            while (auxIndiceNode1 < indice) {
+                auxNode1 = auxNode1->proximo;
+                auxIndiceNode1++;
+            }
+            while (auxIndiceNode2 < indice - 1) {
+                auxNode2 = auxNode2->proximo;
+                auxIndiceNode2++;
+            }
+            novoNode->proximo = auxNode1;
+            auxNode2->proximo = novoNode;
+            quantidadeDeNodes++;
         }
 
     }
@@ -67,16 +113,18 @@ void inserirNodeFim(node **inicio, int novoValor) {
 int main() {
 
     node *inicio = NULL; // A inicialização do ponteiro 'inicio' com NULL é extremamente necessária!
-    int loopMenu, novoValor;
+    int loopMenu, novoValor, indice;
 
     do {
         system("cls");
+        printf("Quantidade de nodes: %d\n", quantidadeDeNodes);
         imprimeNode(inicio);
 
         printf("\nMenu:\n");
         printf("1 - Inserir node no inicio\n");
         printf("2 - Inserir node no fim\n");
-        printf("3 - Sair\n");
+        printf("3 - Inserir node em determinado indice\n");
+        printf("%d - Sair\n", fimMenu);
         printf("\nSua escolha: ");
         scanf("%d", &loopMenu);
 
@@ -92,13 +140,20 @@ int main() {
                 inserirNodeFim(&inicio, novoValor);
                 break;
             case 3:
+                printf("Digite um numero inteiro: ");
+                scanf("%d", &novoValor);
+                printf("Digite o indice desejado: ");
+                scanf("%d", &indice);
+                inserirNode(&inicio, indice, novoValor);
+                break;
+            case fimMenu:
                 printf("Saindo...\n");
                 break;
             default:
                 printf("Opcao invalida!\n");
         }
 
-    } while (loopMenu != 3);
+    } while (loopMenu != 4);
 
     return 0;
     
